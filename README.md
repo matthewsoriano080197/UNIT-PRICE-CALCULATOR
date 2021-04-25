4c3db976d63c4da8b1d83599cd47901c86b77a3f
matthewsoriano080197 <john.soriano080197@my.lsgh.edu.ph>
2021-04-26 07:42:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-4bndv2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-4bndv2)
---
+++ pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  styleUrls: ['home.css']
+})
+export class HomePage {
+  Volume_M: number;
+  Volume_L:number; 
+  Cost_A: number;
+  Cost_B: number;
+  UnitPrice_A: number;
+  UnitPrice_B: number; 
+  Savings: number;
+  FinalPrice: number;
+
+
+  constructor(public navCtrl: NavController) {}
+
+computeCALUMATE() {
+  this.UnitPrice_A = (this.Cost_A/this.Volume_M)*1000;
+  this.UnitPrice_B = this.Cost_B/this.Volume_L; 
+  this.Savings = this.UnitPrice_B - this.UnitPrice_A;
+ 
+  }
+}  
+
